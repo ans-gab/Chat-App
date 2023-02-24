4facda4a870eb5a453fb8691954777cdf4dc955d
superson <474015445@qq.com>
2023-02-24 18:21:14 +0800

    新增发送请求后加载动画图标

--- src/pages/Question/index.js
+++ src/pages/Question/index.js
@@ -1,51 +1,64 @@
 import React, { useState } from 'react'
+import store from '../../store';
 import { Input, Button } from 'antd'
 import axios from 'axios'
 import Reply from '../../commponent/Reply';
-import { changeDiologList, changeReply } from '../../store/actionCreators';
+import ModeMsg from '../../commponent/ModeMsg';
+import { changeDiologList, changeQuestion, changeReply } from '../../store/actionCreators';
 import { connect } from 'react-redux'
 import './index.css'
 
 const Question = (props) => {
-
   const [inputValue, setInputValue] = useState('');
   const [btnmsg, setBtnmsg] = useState('发送');
-
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
-
-  const handleDialogInput = () => {
+  const handleDialogInput = (event) => {
     if (inputValue.trim() === '') {
       return;
     }
-
+    setInputValue(event.target.value);
     const newDiologList = [...props.diologlist, {
       text: inputValue,
       type: 'outgoing',
-      isreply: 0
+      isloading: false
+    }, {
+      type: 'incoming',
+      isloading: true
     }];
+
+    if (props.mode === 0) {
+      props.setQuestion(inputValue)
+      console.log(store.getState().question);
+    }
+    else {
+      props.setQuestion(props.question + '\nQ:' + inputValue)
+      console.log(store.getState().question);
+
+    }
+
     props.setDialogList(newDiologList);
     setInputValue('');
     setBtnmsg('请求中');
     axios.post('https://api.openai.com/v1/completions', {
-      prompt: inputValue, max_tokens: 2048, model: "text-davinci-003", temperature: 0.2
+      prompt: store.getState().question, max_tokens: 2048, model: "text-davinci-003", temperature: 0.2
     }, {
       headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + props.api }
     }).then(res => {
       console.log('res:', res);
-      let text = res.data.choices[0].text.replace("openai:", "").replace("openai：", "").replace(/(^！\n\n)/g, "").replace(/^\n|\n$/g, "")
+      let text = res.data.choices[0].text.replace("openai:", "").replace("openai：", "").replace(/(^！\n\n)/g, "").replace(/^\n|\n$/g, "").replace("A:", "").replace(/(^？\n\n)/g, "")
       console.log('text:', text);
-      props.setDialogList([...newDiologList, { text, type: 'ingoing' }])
+      newDiologList.pop();
+      props.setDialogList([...newDiologList, { text, type: 'ingoing', isloading: false }])
       setBtnmsg('发送')
     })
   };
 
   return (
     <div>
-      <div className='dialog-list'>
-        <Reply />
-      </div>
+      <ModeMsg />
+      <Reply />
       <div className='input-form'>
         <Input.Group compact className='input-form' >
           <Input placeholder='你可以问我任何问题' disabled={btnmsg === "请求中"} style={{ width: 'calc(100% - 5rem)', color: "#00bcd4" }} type="text" value={inputValue} onChange={handleInputChange} onPressEnter={handleDialogInput} />
@@ -59,7 +72,9 @@ const mapStateToProps = state => {
   return {
     isreply: state.isreply,
     diologlist: state.diologlist,
-    api: state.apikey
+    api: state.apikey,
+    mode: state.mode,
+    question: state.question
   }
 }
 const mapDishpatchToProps = dispatch => {
@@ -69,9 +84,11 @@ const mapDishpatchToProps = dispatch => {
     },
     setDialogList: function (ary) {
       dispatch(changeDiologList(ary))
+    },
+    setQuestion: function (text) {
+      dispatch(changeQuestion(text))
     }
-
-  }
+  };
 
 }
 export default connect(mapStateToProps, mapDishpatchToProps)(Question) 
