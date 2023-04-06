b5179988b4d69e100f209adf1af768d47d1a1d80
superson <474015445@qq.com>
2023-04-06 17:48:17 +0800

    调整路由跳转方式，减少调试工具a包含a的报错

--- src/commponent/HomeModel/index.jsx
+++ src/commponent/HomeModel/index.jsx
@@ -3,32 +3,25 @@ import { List, Image } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { example } from '../../common/local-data'
 import './index.css'
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { changeExample } from '../../store/actionCreators'
 
 const HomeModel = (props) => {
+  const navigate = useNavigate()
   return (
     <div id="scrollableDiv" style={{ overflow: 'auto', padding: '0 16px', border: '1px solid rgba(140, 140, 140, 0.35)', }}>
       <List header='官网示例列表'>
         {example.map((item, index) => (
           <List.Item
             key={index}
-            prefix={
-              <Image
-                src={item.imgsrc}
-                fit='cover'
-                width={40}
-                height={40}
-              />
-            }
+            prefix={<Image src={item.imgsrc} fit='cover' width={40} height={40} />}
             description={item.description}
-            onClick={() => { props.setExample(item) }}
+            onClick={() => { props.setExample(item); navigate('/example') }}
           >
-            <NavLink className="example-title" to="/example">{item.title}</NavLink>
+            {item.title}
           </List.Item>
         ))}
       </List>
-
     </div>
   )
 }
